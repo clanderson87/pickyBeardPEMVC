1c2e7a796ff38d48d821a01ef31478f3aef05f2c
Chris Anderson <cl.anderson8@gmail.com>
2016-12-20 14:44:33 -0600

    group model

---
+++ web/models/group.ex
@@ -0,0 +1,15 @@
+defmodule PickyBeardPEMVC.Group do
+  use PickyBeardPEMVC.Web, :model
+
+  schema "groups" do
+    field :name, :string
+    many_to_many :members, PickyBeardPEMVC.User join_through: "user_id"
+    # many_to_many :admins, PickyBeardPEMVC.User join_through: "admins_of"
+  end
+
+  def changeset(struct, params \\%{}) do
+    struct
+    |> cast(params, [:name])
+    |> validate_required([:name])
+  end
+end
